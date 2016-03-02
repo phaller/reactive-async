f3c1a0e4e328b8e57c5edd15fa8d2cb3a370e599
Michael Eichberg <mail@michael-eichberg.de>
2016-03-02 10:37:00 +0100

    fixed the library dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,5 +2,5 @@ import sbt._
 
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "2.2.4" % "test"
-  lazy val opal = "de.opal-project" % "abstract-interpretation-framework_2.11" % "0.0.2-SNAPSHOT"
+  lazy val opal = "de.opal-project" % "abstract-interpretation-framework_2.11" % "0.0.2"
 }
