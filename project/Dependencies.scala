9551a21adbba5b9c160584fe92b1901e19f90ae1
Philipp Haller <hallerp@gmail.com>
2016-04-15 14:46:40 +0200

    Remove obsolete dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,5 +3,4 @@ import sbt._
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "2.2.4" % "test"
   lazy val opal = "de.opal-project" % "abstract-interpretation-framework_2.11" % "0.0.2"
-  lazy val opalCommon = "de.opal-project" % "common" % "0.0.2"
 }
