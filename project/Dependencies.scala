e97cc4db7e8782e6b7b2338aaf1204d76277bf32
Philipp Haller <hallerp@gmail.com>
2018-02-02 23:13:41 +0100

    Only depend on OPAL common for compiling

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,6 +2,7 @@ import sbt._
 
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.0.4" % "test"
-  lazy val opal = "de.opal-project" % "abstract-interpretation-framework_2.12" % "1.0.0"
+  lazy val opalCommon = "de.opal-project" % "common_2.12" % "1.0.0"
+  lazy val opalAI = "de.opal-project" % "abstract-interpretation-framework_2.12" % "1.0.0"
   lazy val scalaMeter = "com.storm-enroute" %% "scalameter" % "0.8.2"
 }
