ecd009398533b887c7c755a78a86193ae0127068
Dominik Helm <helm@cs.tu-darmstadt.de>
2019-05-13 09:48:55 +0200

    Run each setup 3 times

--- core/src/test/scala/com/phaller/rasync/test/opal/ifds/TestTaintAnalysis.scala
+++ core/src/test/scala/com/phaller/rasync/test/opal/ifds/TestTaintAnalysis.scala
@@ -221,7 +221,7 @@ class TestTaintAnalysis(
             }) {
                 println(s"Found flow: $stmt")
             }
-        if (true||(callee.descriptor.returnType eq ObjectType.Class) ||
+        if (true || (callee.descriptor.returnType eq ObjectType.Class) ||
             (callee.descriptor.returnType eq ObjectType.Object) ||
             (callee.descriptor.returnType eq ObjectType.String)) {
             in.collect {
@@ -362,7 +362,7 @@ class TestTaintAnalysis(
      * If forName is called, we add a FlowFact.
      */
     override def nativeCall(statement: Statement, callee: DeclaredMethod, successor: Statement, in: Set[Fact]): Set[Fact] = {
-       /* val allParams = asCall(statement.stmt).allParams
+        /* val allParams = asCall(statement.stmt).allParams
         if (statement.stmt.astID == Assignment.ASTID && in.exists {
             case Variable(index) ⇒
                 allParams.zipWithIndex.exists {
@@ -411,7 +411,7 @@ object TestTaintAnalysisRunner extends FunSuite {
 
     def main(args: Array[String]): Unit = {
 
-        val p0 = Project(new File(args(args.length-1)))//bytecode.RTJar)
+        val p0 = Project(new File(args(args.length - 1))) //bytecode.RTJar)
 
         com.phaller.rasync.pool.SchedulingStrategy
 
@@ -426,58 +426,61 @@ object TestTaintAnalysisRunner extends FunSuite {
             }
         )
 
-        p0.getOrCreateProjectInformationKeyInitializationData(LazyDetachedTACAIKey,
+        p0.getOrCreateProjectInformationKeyInitializationData(
+            LazyDetachedTACAIKey,
             (m: Method) ⇒ new PrimitiveTACAIDomain(p0, m)
         )
 
         PerformanceEvaluation.time {
             val manager = p0.get(FPCFAnalysesManagerKey)
-            manager.runAll(new CallGraphDeserializerScheduler(new File(args(args.length-2))))
+            manager.runAll(new CallGraphDeserializerScheduler(new File(args(args.length - 2))))
         } { t ⇒ println(s"CG took ${t.toSeconds}") }
 
         for (
-            scheduling ← List(/*new DefaultScheduling[IFDSProperty[Fact], (DeclaredMethod, Fact)], new SourcesWithManyTargetsFirst[IFDSProperty[Fact], (DeclaredMethod, Fact)], new SourcesWithManyTargetsLast[IFDSProperty[Fact], (DeclaredMethod, Fact)], new TargetsWithManySourcesFirst[IFDSProperty[Fact], (DeclaredMethod, Fact)], new TargetsWithManySourcesLast[IFDSProperty[Fact], (DeclaredMethod, Fact)], new TargetsWithManyTargetsFirst[IFDSProperty[Fact], (DeclaredMethod, Fact)], new TargetsWithManyTargetsLast[IFDSProperty[Fact], (DeclaredMethod, Fact)],*/ new SourcesWithManySourcesFirst[IFDSProperty[Fact], (DeclaredMethod, Fact)], new SourcesWithManySourcesLast[IFDSProperty[Fact], (DeclaredMethod, Fact)]);
+            scheduling ← List( /*new DefaultScheduling[IFDSProperty[Fact], (DeclaredMethod, Fact)], new SourcesWithManyTargetsFirst[IFDSProperty[Fact], (DeclaredMethod, Fact)], new SourcesWithManyTargetsLast[IFDSProperty[Fact], (DeclaredMethod, Fact)], new TargetsWithManySourcesFirst[IFDSProperty[Fact], (DeclaredMethod, Fact)], new TargetsWithManySourcesLast[IFDSProperty[Fact], (DeclaredMethod, Fact)], new TargetsWithManyTargetsFirst[IFDSProperty[Fact], (DeclaredMethod, Fact)], new TargetsWithManyTargetsLast[IFDSProperty[Fact], (DeclaredMethod, Fact)],*/ new SourcesWithManySourcesFirst[IFDSProperty[Fact], (DeclaredMethod, Fact)], new SourcesWithManySourcesLast[IFDSProperty[Fact], (DeclaredMethod, Fact)]);
             threads ← List(1, 2, 4, /*8,*/ 10, /*16,*/ 20, /*32,*/ 40)
         ) {
             var result = 0
             var analysis: TestTaintAnalysis = null
             var entryPoints: Map[DeclaredMethod, Fact] = null
             var ts: List[Long] = List.empty
-            PerformanceEvaluation.time({
-                implicit val p: Project[URL] = p0 //.recreate(k ⇒ k == PropertyStoreKey.uniqueId || k == DeclaredMethodsKey.uniqueId)
-                //Counter.reset()
+            for (i ← (0 until 3)) {
+                PerformanceEvaluation.time({
+                    implicit val p: Project[URL] = p0 //.recreate(k ⇒ k == PropertyStoreKey.uniqueId || k == DeclaredMethodsKey.uniqueId)
+                    //Counter.reset()
 
-                // From now on, we may access ps for read operations only
-                // We can now start TestTaintAnalysis using IFDS.
-                analysis = new TestTaintAnalysis(threads, scheduling)
+                    // From now on, we may access ps for read operations only
+                    // We can now start TestTaintAnalysis using IFDS.
+                    analysis = new TestTaintAnalysis(threads, scheduling)
 
-                entryPoints = analysis.entryPoints
-                entryPoints.foreach(analysis.forceComputation)
-                analysis.waitForCompletion()
-            }) { t ⇒
+                    entryPoints = analysis.entryPoints
+                    entryPoints.foreach(analysis.forceComputation)
+                    analysis.waitForCompletion()
+                }) { t ⇒
 
-                result = 0
-                for {
-                    e ← entryPoints
-                    fact ← analysis.getResult(e).flows.values.flatten.toSet[Fact]
-                } {
-                    fact match {
-                        case FlowFact(flow) ⇒ result += 1;println(s"flow: "+flow.map(_.toJava).mkString(", "))
-                        case _           ⇒
+                    result = 0
+                    for {
+                        e ← entryPoints
+                        fact ← analysis.getResult(e).flows.values.flatten.toSet[Fact]
+                    } {
+                        fact match {
+                            case FlowFact(flow) ⇒
+                                result += 1; println(s"flow: "+flow.map(_.toJava).mkString(", "))
+                            case _              ⇒
+                        }
                     }
+                    //println(Counter.toString)
+                    println(s"NUM RESULTS =  $result")
+                    println(s"time = ${t.toSeconds}")
+
+                    ts ::= t.timeSpan
                 }
-                //println(Counter.toString)
-                println(s"NUM RESULTS =  $result")
-                println(s"time = ${t.toSeconds}")
-
-                ts ::= t.timeSpan
             }
-
             val lastAvg = ts.sum / ts.size
             println(s"AVG,${scheduling.getClass.getSimpleName},$threads,$lastAvg")
         }
 
- /*       for (
+        /*       for (
             scheduling ← List(new DefaultScheduling[IFDSProperty[Fact], (DeclaredMethod, Fact)], new SourcesWithManyTargetsFirst[IFDSProperty[Fact], (DeclaredMethod, Fact)], new SourcesWithManyTargetsLast[IFDSProperty[Fact], (DeclaredMethod, Fact)], new TargetsWithManySourcesFirst[IFDSProperty[Fact], (DeclaredMethod, Fact)], new TargetsWithManySourcesLast[IFDSProperty[Fact], (DeclaredMethod, Fact)], new TargetsWithManyTargetsFirst[IFDSProperty[Fact], (DeclaredMethod, Fact)], new TargetsWithManyTargetsLast[IFDSProperty[Fact], (DeclaredMethod, Fact)], new SourcesWithManySourcesFirst[IFDSProperty[Fact], (DeclaredMethod, Fact)], new SourcesWithManySourcesLast[IFDSProperty[Fact], (DeclaredMethod, Fact)]);
             threads ← List(1, 2, 4, 8, 10, 16, 20, 32, 40)
         ) {
